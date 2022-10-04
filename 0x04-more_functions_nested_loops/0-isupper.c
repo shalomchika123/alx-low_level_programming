d2e7224d1f299473c85dd43379ff29bae576f22d
chika <shalomchika01@gmail.com>
2022-10-04 13:02:29 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *
+ * main - check the code
+ * 
+ * Return - always 0 or 1
+ */
+
+int _isupper(int c)
+{	
+	if (c >= 'A' && c <= 'Z') 
+		return (1);
+	else
+		return (0);
+}
+		
